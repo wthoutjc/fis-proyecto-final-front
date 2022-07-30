2335b4c2ad89f53294d2113100694b856d0a2588
wthoutjc <juancamilorr2012@hotmail.com>
2022-07-29 21:34:15 -0500

    fix bug

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
+  const token = req.cookies.get("__Secure-next-auth.session-token") || req.cookies.get("next-auth.session-token");
+
   if (req.nextUrl.pathname.startsWith("/auth")) {
-    if (req.cookies.get("next-auth.session-token")) {
+    if (token) {
       const url = req.nextUrl.clone();
       url.pathname = "/home";
       return NextResponse.redirect(url);
@@ -15,7 +17,7 @@ const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
     req.nextUrl.pathname.startsWith("/home") ||
     req.nextUrl.pathname.startsWith("/new")
   ) {
-    if (!req.cookies.get("next-auth.session-token")) {
+    if (!token) {
       const url = req.nextUrl.clone();
       url.pathname = "/";
       return NextResponse.redirect(url);
