1c56ae2691e013f82ca1cc0ea2109ab4cb758408
wthoutjc <juancamilorr2012@hotmail.com>
2022-08-14 18:28:55 -0500

    fix bugs

--- components/entries/RecentlyCreated.tsx
+++ components/entries/RecentlyCreated.tsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import { IDocument } from "../../interfaces";
 
 // Component
 import { Card } from "../entries";
@@ -15,67 +14,13 @@ const RecentlyCreated = () => {
         flexDirection="column"
         sx={{ maxHeight: "750px", overflow: "auto" }}
       >
-        {DATA.map((document, i) => (
+        En construcción
+        {/* {DATA.map((document, i) => (
           <Card key={i} document={document} />
-        ))}
+        ))} */}
       </Box>
     </Box>
   );
 };
 
 export { RecentlyCreated };
-
-const DATA: IDocument[] = [
-  {
-    id: "1",
-    typeId: "ISBN",
-    title: "Test 1",
-    type: "libro",
-    ownerName: "Pepito 1 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 1 Perez",
-    dateCreated: 12344,
-    dateLastModified: 12344,
-    autors: ["Test"],
-    editorial: "Test 1",
-  },
-  {
-    id: "2",
-    typeId: "ISBN",
-    title: "Test 2",
-    type: "libro",
-    ownerName: "Pepito 2 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 2 Perez",
-    dateCreated: 12344,
-    dateLastModified: 12344,
-    autors: ["Test"],
-    editorial: "Test 2",
-  },
-  {
-    id: "3",
-    typeId: "ISBN",
-    title: "Test 3",
-    type: "libro",
-    ownerName: "Pepito 3 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 3 Perez",
-    dateCreated: 32344,
-    dateLastModified: 32344,
-    autors: ["Test"],
-    editorial: "Test 3",
-  },
-  {
-    id: "4",
-    typeId: "ISBN",
-    title: "Test 4",
-    type: "libro",
-    ownerName: "Pepito 4 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 4 Perez",
-    dateCreated: 42344,
-    dateLastModified: 42344,
-    autors: ["Test"],
-    editorial: "Test 4",
-  },
-];
--- pages/document/index.tsx
+++ pages/document/index.tsx
@@ -16,7 +16,7 @@ import ArticleIcon from "@mui/icons-material/Article";
 import { CTable } from "../../components/ui/table";
 
 // Interfaces
-import { IDocument } from "../../interfaces";
+import { IPublication } from "../../interfaces";
 
 const DocumentPage = () => {
   const dispatch = useAppDispatch();
@@ -96,7 +96,7 @@ const DocumentPage = () => {
             Documentos
           </Typography>
           <Box>
-            <CTable data={DATA} />
+            <CTable data={[]} />
           </Box>
         </Box>
       </ConnectedLayout>
@@ -105,58 +105,3 @@ const DocumentPage = () => {
 };
 
 export default DocumentPage;
-
-const DATA: IDocument[] = [
-  {
-    id: "1",
-    typeId: "ISBN",
-    title: "Test 1",
-    type: "libro",
-    ownerName: "Pepito 1 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 1 Perez",
-    dateCreated: 12344,
-    dateLastModified: 12344,
-    autors: ["Test"],
-    editorial: "Test 1",
-  },
-  {
-    id: "2",
-    typeId: "ISBN",
-    title: "Test 2",
-    type: "libro",
-    ownerName: "Pepito 2 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 2 Perez",
-    dateCreated: 12344,
-    dateLastModified: 12344,
-    autors: ["Test"],
-    editorial: "Test 2",
-  },
-  {
-    id: "3",
-    typeId: "ISBN",
-    title: "Test 3",
-    type: "libro",
-    ownerName: "Pepito 3 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 3 Perez",
-    dateCreated: 32344,
-    dateLastModified: 32344,
-    autors: ["Test"],
-    editorial: "Test 3",
-  },
-  {
-    id: "4",
-    typeId: "ISBN",
-    title: "Test 4",
-    type: "libro",
-    ownerName: "Pepito 4 Perez",
-    ownerEmail: "pepito@correo.com",
-    lastModifiedName: "Pepito 4 Perez",
-    dateCreated: 42344,
-    dateLastModified: 42344,
-    autors: ["Test"],
-    editorial: "Test 4",
-  },
-];
